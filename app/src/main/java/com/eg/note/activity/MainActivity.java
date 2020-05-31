89e6276955fd8ab5ada014822cdf9371472d67c6
makewheels <33256842+makewheels@users.noreply.github.com>
2020-05-31 13:10:13 +0800

    //test for some modify

--- app/src/main/java/com/eg/note/activity/MainActivity.java
+++ app/src/main/java/com/eg/note/activity/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//test for some modify
         initView();
         addListeners();
         noteDao = GreenDaoApplication.getInstance().getDaoSession().getNoteDao();
